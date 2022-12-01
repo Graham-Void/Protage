8b90c7a5bb51f32098641d014ce76677fc3908c9
Graham-Void <111210697+Graham-Void@users.noreply.github.com>
2022-12-01 07:02:50 -0800

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -1,31 +1,11 @@
-local link1 = 'https://raw.githubusercontent.com'
-local link2 = 'Graham-Void/Protage/'
-local link3 = 'main/White.lua'
-
-local final_wl = link1 .. '/' .. link2 .. link3
-
-local link4 = 'https://raw.githubusercontent.com'
-local link5 = 'Graham-Void/Protage/'
-local link6 = 'main/banned.lua'
-
-local final_bl = link4 .. '/' .. link5 .. link6
-
-
-local userkey = final_wl
-local blacklist = final_bl
+userkey = "https://raw.githubusercontent.com/Graham-Void/Protage/main/White.lua"
+blacklist = "https://raw.githubusercontent.com/Graham-Void/Protage/main/banned.lua"
 key = game:HttpGet(userkey, true)
 banned = game:HttpGet(blacklist, true)
 if string.find(key,_G.Key) then
-
-    https://raw.githubusercontent.com/Graham-Void/Protage/main/script.lua
-    local l1 = 'https://raw.githubusercontent.com'
-    local l2 = 'Graham-Void/Protage/'
-    local l3 = 'main/script.lua'
-
-    local final = l1 .. '/' .. l2 .. l3
-    loadstring(game:HttpGet((final),true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Graham-Void/Protage/main/script.lua", true))()
 elseif string.find(blacklist,_G.Key) then
-game.Players.LocalPlayer:kick("What are u doing ?")
+    game.Players.LocalPlayer:kick("What are u doing ?")
 else
-game.Players.LocalPlayer:kick("What are u doing ?")
+    game.Players.LocalPlayer:kick("What are u doing ?")
 end
