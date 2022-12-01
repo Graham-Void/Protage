d4a41dc6307771e2e6a86e5fa54dd47fcf4630f8
Graham-Void <111210697+Graham-Void@users.noreply.github.com>
2022-12-01 07:22:59 -0800

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -3,19 +3,10 @@ blacklist = "https://raw.githubusercontent.com/Graham-Void/Protage/main/banned.l
 key = game:HttpGet(userkey, true)
 banned = game:HttpGet(blacklist, true)
 
-if _G.key == '' then
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
-end
-if _G.key == ' ' then
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
-end
-if _G.key == nil then
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
-end
-if string.find(key,_G.Key) then
+if string.find(key,wl_key) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Graham-Void/Protage/main/script.lua", true))()
-elseif string.find(blacklist,_G.Key) then
-    game.Players.LocalPlayer:Kick("You are banned!")
+elseif string.find(blacklist,wl_key) then
+    game.Players.LocalPlayer:Kick("You are blacklisted!, contact graham")
 else
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
+    game.Players.LocalPlayer:Kick("You are not whitelisted!, contact graham")
 end
