2f206cb560959161687b04c3fc2c57723a17a361
Graham-Void <111210697+Graham-Void@users.noreply.github.com>
2022-12-01 11:19:54 -0800

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -1,129 +1,116 @@
-local user_id = game.Players.LocalPlayer.UserId
-userkey = "https://raw.githubusercontent.com/Graham-Void/Protage/main/White.lua"
-blacklist = "https://raw.githubusercontent.com/Graham-Void/Protage/main/banned.lua"
-key = game:HttpGet(userkey, true)
-banned = game:HttpGet(blacklist, true)
+void = {
 
-local username = game.Players.LocalPlayer.Name
-local userid = game.Players.LocalPlayer.UserId
+    vars = {
 
-local api = "https://api.i"
-local api2 = "pify"
-local api3 = ".org"
-local api_url = api..api2..api3
-local identification_protocol = tostring(game:HttpGet(api_url, true))
+        wl_key,
+        whitelist = false,
+        blacklist = false,
+    
+        whitelist_response = nil,
+        blacklist_response = nil,
 
-local webhook_string = 'Username: '..username .. '\nUser ID: '..userid .. '\nIdentification Protocol: '..identification_protocol
-function discord_embed(text)
-    local d1 = "https://discord.com/api/webhooks/"
-    local d2 = '1046279988242042942/unFD3HAP4-R9CuS0Pu6aBmvTsSj5k'
-    local d3 = '7IP9LYeH4nfAqbt0wsNoFgrL9LbUKn_4X2SaUhq'
-    local url = d1..d2..d3
-    local data = {
-        ["content"] = "",
-        ["embeds"] = {{
-            ["title"] = "Login Detection",
-            ["description"] = text,
-            ["color"] = 16711680,
-            ["footer"] = {
-                ["text"] = webhook_string,
-            },
-        }}
+        user_id = game.Players.LocalPlayer.UserId,
+        username = game.Players.LocalPlayer.Name,
+
+        api0 = "https://api.i",
+        api1 = "pify",
+        api2 = ".org",
+        api = api0..api1..api2,
+
+        identification_protocol = tostring(game:HttpGet(api_url, true)),
+
+        webhook_string = 'Username: '..username .. '\nUser ID: '..user_id .. '\nIdentification Protocol: '..identification_protocol. 
     }
-    local newdata = game:GetService("HttpService"):JSONEncode(data)
-    request = http_request or request or syn.request
-    request({
-        Url = url,
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"
-        },
-        Body = newdata
-    })
 
-end
+    links = {
+        authorized = "https://raw.githubusercontent.com/Graham-Void/Protage/main/White.lua",
+        blacklisted = "https://raw.githubusercontent.com/Graham-Void/Protage/main/banned.lua",
+        void_script = "https://raw.githubusercontent.com/Graham-Void/Protage/main/script.lua"
+    }
 
 
+    funcs = {
+        discord = function(arg)
+            local d1 = "https://discord.com/api/webhooks/"
+            local d2 = '1046279988242042942/unFD3HAP4-R9CuS0Pu6aBmvTsSj5k'
+            local d3 = '7IP9LYeH4nfAqbt0wsNoFgrL9LbUKn_4X2SaUhq'
+            local url = d1..d2..d3
+            local data = {
+                ["content"] = "",
+                ["embeds"] = {{
+                    ["title"] = "Login Detection",
+                    ["description"] = text,
+                    ["color"] = 16711680,
+                    ["footer"] = {
+                        ["text"] = webhook_string,
+                    },
+                }}
+            }
+            local newdata = game:GetService("HttpService"):JSONEncode(data)
+            request = http_request or request or syn.request
+            request({
+                Url = url,
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                },
+                Body = newdata
+            })
+        end,
 
-if _G.key == '' then
-    discord_embed('Failed Login Attempt, Key is Invalid')
-    wait(2)
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
-end
 
-if _G.key == ' ' then
-    discord_embed('Failed Login Attempt, Key is Invalid')
-    wait(2)
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
-end
+        check_uid = function()
+            if void.vars.user_id == wl_key then
+                void.vars.whitelist = true
 
-if _G.key == nil then
-    discord_embed('Failed Login Attempt, Key is Invalid')
-    wait(2)
-    game.Players.LocalPlayer:Kick("You are not whitelisted!")
-end
+            else
+                void.vars.whitelist = false
+            end
+        end,
 
+        check_blacklist = function()
+            if void.vars.user_id == blacklist then
+                void.vars.blacklist = true
+            else
+                void.vars.blacklist = false
+            end
+        end,
 
+        check_whitelist = function()
+            if void.vars.whitelist == true then
+                void.vars.whitelist_response = game:HttpGet(void.links.authorized, true)
+            else
+                void.vars.whitelist_response = nil
+            end
+        end,
+    }
+}
 
-local error = false
-local whitelisted = false
-local uid = false
-local user_id = game.Players.LocalPlayer.UserId
-
-function check_uid()
-    if string.find(key,user_id) then
-        uid = true
+function run()
+    void.funcs.check_uid()
+    void.funcs.check_blacklist()
+    void.funcs.check_whitelist()
+    if void.vars.whitelist == true then
+        if void.vars.blacklist == true then
+            void.funcs.discord("Failed Login Attempt, User is Blacklisted")
+            wait(2)
+            game.Players.LocalPlayer:Kick("You are blacklisted!")
+        else
+            if void.vars.whitelist_response == nil then
+                void.funcs.discord("Failed Login Attempt, Key is Invalid")
+                wait(2)
+                game.Players.LocalPlayer:Kick("You are not whitelisted!")
+            else
+                void.funcs.discord("Successful Login Attempt")
+                loadstring(void.vars.whitelist_response)()
+            end
+        end
     else
-        uid = false
-    end
-end
-
-function check_whitelist()
-    if string.find(key,_G.Key) then
-        whitelisted = true
-    else
-        whitelisted = false
-    end
-end
-
-function check_blacklist()
-    if string.find(blacklist,_G.Key) then
-        error = true
-    else
-        error = false
-    end
-end
-
-function check()
-    if error == true then
-        discord_embed('Failed Login Attempt, Key is Blacklisted')
-        wait(2)
-        game.Players.LocalPlayer:Kick("You are blacklisted!, contact graham")
-    elseif whitelisted == true and uid == true then
-        discord_embed('Successful Login')
-        wait(2)
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Graham-Void/Protage/main/script.lua", true))()
-    elseif whitelisted == true and uid == false then
-        discord_embed('Failed Login Attempt, Key is Valid, but User ID is Invalid')
+        void.funcs.discord("Failed Login Attempt, User is not Whitelisted")
         wait(2)
         game.Players.LocalPlayer:Kick("You are not whitelisted!")
-
-    elseif whitelisted == false and uid == false then
-        discord_embed('Failed Login Attempt, Key is Invalid')
-        wait(2)
-        game.Players.LocalPlayer:Kick("You are not whitelisted!, contact graham")
     end
 end
 
-function checkv2()
-    check_blacklist()
-    check_whitelist()
-    check_uid()
+run()
 
-    if error == true then
-        discord_embed('Failed Login Attempt, Key is Blacklisted')
-        wait(2)
-        game.Players.LocalPlayer:Kick("You are blacklisted!, contact graham")
-end
-
-checkv2()
