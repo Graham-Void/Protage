8b4e6446de24a65d101eb1b978af50e462500da0
Graham-Void <111210697+Graham-Void@users.noreply.github.com>
2022-12-01 11:49:21 -0800

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -1,10 +1,8 @@
 
-local wl_key
+local wl_key;
 
-userkey = "https://raw.githubusercontent.com/Graham-Void/Protage/main/White.lua"
-blacklist = "https://raw.githubusercontent.com/Graham-Void/Protage/main/banned.lua"
-key = game:HttpGet(userkey, true)
-banned = game:HttpGet(blacklist, true)
+local key = game:HttpGet("https://raw.githubusercontent.com/Graham-Void/Protage/main/White.lua", true)
+local banned = game:HttpGet("https://raw.githubusercontent.com/Graham-Void/Protage/main/banned.lua", true)
 local user_id = game.Players.LocalPlayer.UserId
 
 if string.find(key,wl_key) then
@@ -17,7 +15,7 @@ if string.find(key,wl_key) then
         wait(2)
         game.Players.LocalPlayer:Kick("You are not whitelisted!")
     end
-elseif string.find(blacklist,wl_key) then
+elseif string.find(banned,wl_key) then
     discord_embed('Failed Login Attempt, Key is Blacklisted')
     wait(2)
     game.Players.LocalPlayer:Kick("You are blacklisted!, contact graham")
